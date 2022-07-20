a070b1f26bdf15b8e69f8e94804825d0dff150b8
Irek Iwanca <iiwanca@gmail.com>
2022-07-20 12:59:28 +0200

    User interface & register method created

---
+++ src/interfaces/user.ts
@@ -0,0 +1,9 @@
+export interface RegisterUser {
+    id: string;
+    email: string;
+}
+
+export type RegisterUserResponse = RegisterUser | {
+    isSuccessful: false;
+    message: string;
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,29 @@
 import { Injectable } from '@nestjs/common';
+import {RegisterDto} from "./dto/register.dto";
+import {RegisterUserResponse} from "../interfaces/user";
+import {User} from "./user.entity";
+import {hashPwd} from "../utils/hash-pwd";
 
 @Injectable()
-export class UserService {}
+export class UserService {
+
+    filter(user: User): RegisterUserResponse {
+        const {id, email} = user;
+        return {id, email};
+    }
+
+    async register(newUser: RegisterDto): Promise<RegisterUserResponse> {
+        const user = new User();
+        user.email = newUser.email;
+        user.pwdHash = hashPwd(newUser.pwd);
+
+        if (await User.findOne({where: {email: user.email}})) {
+            return {
+                isSuccessful: false,
+                message: `Sorry, user with email: '${user.email}' already exist! Please provide a different email.`
+            }
+        }
+        await user.save();
+        return this.filter(user);
+    }
+}
