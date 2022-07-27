e282a6c98be6e81209d395f00d64ea39749ea607
Irek Iwanca <iiwanca@gmail.com>
2022-07-27 15:34:34 +0200

    Endpoints: Student & Students created as per FE specs

--- src/interfaces/students.ts
+++ src/interfaces/students.ts
@@ -1,11 +1,8 @@
 export interface Student {
-  id: string;
   courseCompletion: number;
   courseEngagement: number;
   projectDegree: number;
   teamProjectDegree: number;
-  bonusProjectUrls: string;
-  status: number;
 }
 
 export type AllStudentsResponse = Student[];
--- src/students/students.service.ts
+++ src/students/students.service.ts
@@ -1,16 +1,31 @@
-import { Injectable } from '@nestjs/common';
+import {Inject, Injectable} from '@nestjs/common';
 import {Students} from "./students.entity";
 import {AllStudentsResponse} from "../interfaces/students";
 import {Student} from "../interfaces/students";
+import {DataSource} from "typeorm";
 
 @Injectable()
 export class StudentsService {
 
+    constructor(
+        @Inject(DataSource) private datasource: DataSource,
+    ) {
+    }
+
     async getAllStudents(): Promise<AllStudentsResponse> {
-        return await Students.find();
+        return await this.datasource
+            .createQueryBuilder(Students, 'students')
+            .select(['students.courseCompletion', 'students.courseEngagement', 'students.projectDegree', 'students.teamProjectDegree', 'studentsProfile.firstName', 'studentsProfile.lastName', 'studentsProfile.targetWorkCity', 'studentsProfile.expectedSalary', 'studentsProfile.canTakeApprenticeship', 'studentsProfile.monthsOfCommercialExp', 'studentsProfile.workExperience'])
+            .leftJoin('students.studentsProfile', 'studentsProfile')
+            .execute()
     }
 
     async getOneStudent(id: string): Promise<Student> {
-        return await Students.findOne({where: {id}, relations: ['studentsProfile']})
+        return await this.datasource
+            .createQueryBuilder(Students, 'students')
+            .where('students.id = :studentsId', {studentsId: id})
+            .select(['students.courseCompletion', 'students.courseEngagement', 'students.projectDegree', 'students.teamProjectDegree', 'studentsProfile.firstName', 'studentsProfile.lastName', 'studentsProfile.targetWorkCity', 'studentsProfile.expectedSalary', 'studentsProfile.canTakeApprenticeship', 'studentsProfile.monthsOfCommercialExp', 'studentsProfile.workExperience'])
+            .leftJoin('students.studentsProfile', 'studentsProfile')
+            .execute()
     }
 }
