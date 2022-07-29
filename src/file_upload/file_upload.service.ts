50218ce094376d1132e1294d9a902edd81f0cf13
Gabriel Pazdan <gabriel.pazdan1@gmail.com>
2022-07-29 18:43:21 +0200

    Added file_upload module with controller and service. Implemented csv file upload to the server with reading it's data for class Student.

---
+++ src/file_upload/file_upload.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@nestjs/common';
+import { CsvParser } from 'nest-csv-parser'
+import { Readable } from "stream";
+
+class Student {
+  imie: string;
+  nazwisko: string;
+  rola: string;
+}
+
+@Injectable()
+export class FileUploadService {
+
+  constructor(
+    private readonly csvParser: CsvParser
+  ) {
+  }
+
+  bufferToStream(buffer: Buffer) {
+    const stream = new Readable();
+    stream.push(buffer);
+    stream.push(null);
+    return stream;
+  }
+
+  async readCsv(file: Express.Multer.File) {
+    const buffer = file.buffer;
+    const stream = await this.bufferToStream(buffer);
+
+    const csvData = await this.csvParser.parse(stream, Student);
+
+    console.log(csvData);
+
+    return csvData;
+  }
+}
